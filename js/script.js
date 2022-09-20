2f28e1b088bf860f3c15e850db28392b223632c8
mateussdev <mateusbs427@gmail.com>
2022-09-20 16:06:21 -0300

    :pencil: menu resposive for large screens and changeHeaderWhenScroll

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ function openMenu() {
 function closeMenu() {
   menu.classList.remove('show')
 }
+
+// adicionar sombra ao header quando der scroll na página
+// add shadow to header when scrolling page
+const nav = document.querySelector('nav')
+const navHeight = nav.offsetHeight
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    nav.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    nav.classList.remove('scroll')
+  }
+}
+
+// When scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+})
